965515f27dc4ba9f8d3a48ae76245bc1fd333dc6
dotnetking <companytheexplorer@gmail.com>
2025-01-18 08:49:57 +0530

    Add files via upload

---
+++ RateCalculationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class RateCalculationController : Controller
+    {
+        // Action to return the view with initial data
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Action to handle AJAX request and calculate Total
+        [HttpPost]
+        public JsonResult CalculateTotal(RateCalculationModel model)
+        {
+            // Calculate Total (RatePerHour * NumberOfHours)
+            model.Total = model.RatePerHour * model.NumberOfHours;
+
+            // Return the calculated Total as JSON
+            return Json(model);
+        }
+    }
+    public class RateCalculationModel
+    {
+        public decimal RatePerHour { get; set; }
+        public decimal NumberOfHours { get; set; }
+        public decimal Total { get; set; }
+    }
+}
